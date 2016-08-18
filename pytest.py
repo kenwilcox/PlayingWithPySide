815f40bdc814aa90455f19b7fdf679d23704b66d
Ken Wilcox <wilcoxk@medamerica.com>
2016-08-18 16:38:42 -0600

    Making sure it works

---
+++ pytest.py
@@ -0,0 +1,19 @@
+#!python2
+
+import sys
+from PySide.QtCore import Qt
+from PySide.QtGui import QApplication, QLabel
+
+if __name__ == '__main__':
+	myApp = QApplication(sys.argv)
+
+	appLabel = QLabel()
+	appLabel.setText("Hello, World!!!\nLook at my first app using PySide")
+	appLabel.setAlignment(Qt.AlignCenter)
+	appLabel.setWindowTitle("My First PySide Application")
+	appLabel.setGeometry(300, 300, 250, 175)
+
+	appLabel.show()
+
+	myApp.exec_()
+	sys.exit()
