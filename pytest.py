be61b4e037d972663e3f8c0dba90751d15c5e52e
Ken Wilcox <wilcoxk@medamerica.com>
2016-08-18 16:43:52 -0600

    making sure we deal with an Exception

--- pytest.py
+++ pytest.py
@@ -7,13 +7,17 @@ from PySide.QtGui import QApplication, QLabel
 if __name__ == '__main__':
 	myApp = QApplication(sys.argv)
 
-	appLabel = QLabel()
-	appLabel.setText("Hello, World!!!\nLook at my first app using PySide")
-	appLabel.setAlignment(Qt.AlignCenter)
-	appLabel.setWindowTitle("My First PySide Application")
-	appLabel.setGeometry(300, 300, 250, 175)
+	try:
+		appLabel = QLabel()
+		appLabel.setText("Hello, World!!!\nLook at my first app using PySide")
+		appLabel.setAlignment(Qt.AlignCenter)
+		appLabel.setWindowTitle("My First PySide Application")
+		appLabel.setGeometry(300, 300, 250, 175)
 
-	appLabel.show()
+		appLabel.show()
 
-	myApp.exec_()
-	sys.exit()
+		myApp.exec_()
+		sys.exit()
+	except NameError:
+		print "Name Error:", sys.exc_info()[1]
+		pass
