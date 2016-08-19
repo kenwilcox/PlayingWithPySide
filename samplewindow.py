c5f4203af84b4b785991a4f89492b9c00e16ba39
Ken Wilcox <wilcoxk@medamerica.com>
2016-08-19 14:18:34 -0600

    Different icon states

--- samplewindow.py
+++ samplewindow.py
@@ -1,7 +1,7 @@
 #!python3.4
 
 import sys
-from PySide.QtGui import QApplication, QWidget, QIcon
+from PySide.QtGui import QApplication, QWidget, QIcon, QLabel
 
 class SampleWindow(QWidget):
     """ Our main window class
@@ -21,11 +21,31 @@ class SampleWindow(QWidget):
         appIcon = QIcon('pyside_logo.png')
         self.setWindowIcon(appIcon)
 
+    def setIconModes(self):
+        myIcon1 = QIcon('pyside_logo.png')
+        myLabel1 = QLabel('sample', self)
+        pixmap1 = myIcon1.pixmap(50, 50, QIcon.Active, QIcon.On)
+        myLabel1.setPixmap(pixmap1)
+
+        myIcon2 = QIcon('pyside_logo.png')
+        myLabel2 = QLabel('sample', self)
+        pixmap2 = myIcon2.pixmap(50, 50, QIcon.Disabled, QIcon.Off)
+        myLabel2.setPixmap(pixmap2)
+        myLabel2.move(50, 0)
+
+        myIcon3 = QIcon('pyside_logo.png')
+        myLabel3 = QLabel('sample', self)
+        pixmap3 = myIcon3.pixmap(50, 50, QIcon.Selected, QIcon.On)
+        myLabel3.setPixmap(pixmap3)
+        myLabel3.move(100, 0)
+
+
 if __name__ == '__main__':
     try:
         myApp = QApplication(sys.argv)
         myWindow = SampleWindow()
         myWindow.setIcon()
+        myWindow.setIconModes()
         myWindow.show()
         myApp.exec_()
         sys.exit(0)
